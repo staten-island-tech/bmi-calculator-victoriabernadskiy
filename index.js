c0361d2c231558568091ec021d479eaf843127b9
victoriabernadskiy <vbernadskiy0531@gmail.com>
2023-10-05 22:51:53 -0400

    done

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-function findBMI() {
-    var height = x;
-    var weight = y;
-    
-    if (height <= 0 || weight <= 0 || isNaN(height) || isNaN(weight)) {
-      alert('Please fill all fields with valid numbers');
-      return;
-    }
-    
-    height = height / 100;
-    var bmi = weight / (height * height);
-    showResult(bmi);
+function calcbmi(weight, height) {
+  const bmi = (weight / ((height/100)**2)).toFixed(2);
+  console.log(`Bmi = ${bmi}`)
+  if (bmi <= 18.5) {
+    return "Underweight";
+  } else if (bmi <= 25.0) {
+
+    return "Normal";
+  } else if (bmi <= 30.0) {
+
+    return "Overweight";
+  } else if (bmi > 30) {
+    return "Obese";
+  }
 }
+
+function outputbmi(bmi, name) {
+  console.log(`${name}, you are ${bmi}`)
+}
+
+
+let person_obj = {
+  name: prompt("Enter your name"),
+  w: prompt("Enter your weight (kg)"),
+  h: prompt("Enter your height (cm)")
+}
+
+bmi = calcbmi(person_obj.w, person_obj.h);
+outputbmi(bmi, person_obj.name)
